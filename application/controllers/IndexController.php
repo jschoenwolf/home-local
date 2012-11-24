4ffac6c84a68855c495b7f5d1a725aaa62c202b7
John Schoenwolf <jschoenwolf@qwest.net>
2012-11-24 03:13:00 -0700

    Clean up

--- application/controllers/IndexController.php
+++ application/controllers/IndexController.php
@@ -13,7 +13,6 @@ class IndexController extends Zend_Controller_Action
 
     public function init()
     {
-
         $this->message = $this->getHelper('FlashMessenger');
         $this->session = new Zend_Session_Namespace('home');
 
@@ -24,42 +23,6 @@ class IndexController extends Zend_Controller_Action
 
     public function indexAction()
     {
-        $this->_helper->layout->disableLayout();
-        //id3 options
-//        $options  = array("version"  => 3.0, "encoding" => Zend_Media_Id3_Encoding::ISO88591, "compat"   => true);
-        //path to collection
-//        $path     = APPLICATION_PATH . '/../public/Media/Music/';//Currently Approx 2000 files
-        //inner iterator
-//        $dir      = new RecursiveDirectoryIterator($path, RecursiveDirectoryIterator::SKIP_DOTS);
-        //iterator
-//        $iterator = new RecursiveIteratorIterator($dir, RecursiveIteratorIterator::SELF_FIRST);
-        /*
-         * Zend_Layout in its current implementation accumulates whole action output inside itself.
-         * This fact hampers out intention to gradually output the result.
-         * What we do here is we defer execution of our intensive calculation in form of callback into the Postpone plugin.
-         * The scenario is:
-         * 1. Application started
-         * 2. Layout is started
-         * 3. Action gets executed (except callback) and its output is collected by layout.
-         * 4. Layout output goes to response.
-         * 5. Postpone::postDispatch outputs first part of the response (without the tail).
-         * 6. Postpone::postDispatch calls the callback. Its output goes stright to browser.
-         * 7. Postpone::postDispatch prints the tail.
-         */
-//        $this->getFrontController()
-//            ->registerPlugin(new Jgs_Controller_Plugin_Postpone(function () {
-//                        /*
-//                         * A calculation immigration
-//                         * Put your actual calculations here.
-//                         */
-                        echo str_repeat(" ", 1500);
-                        foreach (range(1, 5) as $x) {
-                            echo "<p>$x</p><br />";
-                            usleep(500000);
-                            ob_flush();
-                            flush();
-                        }
-//                    }), 1000);
 
     }
 
@@ -72,7 +35,7 @@ class IndexController extends Zend_Controller_Action
             if ($form->isValid($this->getRequest()->getPost())) {
                 $data = $form->getValues();
 
-                $user = new Application_Model_User($data);
+                $user   = new Application_Model_User($data);
                 $mapper = new Application_Model_Mapper_User();
 
                 $save = $mapper->saveUser($user);
@@ -92,17 +55,17 @@ class IndexController extends Zend_Controller_Action
 
         if ($this->getRequest()->isPost()) {
             if ($form->isValid($this->getRequest()->getPost())) {
-                $data = $form->getValues();
+                $data        = $form->getValues();
                 $authAdapter = new Jgs_Auth_Adapter($data['name'], $data['password']);
             } else {
-                $this->view->form = $form;
+                $this->view->form   = $form;
                 $this->view->errors = $form->getMessages();
             }
             //authenticate
-            $result = $authAdapter->authenticate();
+            $result             = $authAdapter->authenticate();
             if ($result->isValid()) {
                 //store the user object
-                $auth = Zend_Auth::getInstance();
+                $auth    = Zend_Auth::getInstance();
                 $storage = $auth->getStorage();
                 $storage->write($authAdapter->getUser());
                 $this->message->addMessage('Welcome');
