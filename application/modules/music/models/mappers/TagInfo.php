917bbbcb20a3783b16f5a74ccf1632f27433d89a
John Schoenwolf <jschoenwolf@qwest.net>
2013-01-11 02:53:44 -0700

    Working on CSS

--- application/modules/music/models/mappers/TagInfo.php
+++ application/modules/music/models/mappers/TagInfo.php
@@ -27,7 +27,7 @@ class Music_Model_Mapper_TagInfo extends Jgs_Model_Mapper_Abstract
 
     protected function createEntity($row)
     {
-        $row = (object) $row;
+        $row  = (object) $row;
         $data = array(
             'id'               => NULL,
             'album'            => $row->album,
@@ -53,21 +53,22 @@ class Music_Model_Mapper_TagInfo extends Jgs_Model_Mapper_Abstract
         $this->data = $this->getID3->analyze($this->fileToScan);
         // Exit here on error
         if (isset($this->data['error'])) {
+
             return array('error' => $this->data['error']);
         }
-        $this->result['filename'] = (isset($this->data['filename']) ? $this->data['filename'] : '' );
-        $this->result['album'] = (isset($this->data['tags_html']['id3v2']['album'][0]) ? $this->data['tags_html']['id3v2']['album'][0] : '');
-        $this->result['artist'] = (isset($this->data['tags_html']['id3v2']['artist'][0]) ? $this->data['tags_html']['id3v2']['artist'][0] : '');
-        $this->result['bitrate'] = (isset($this->data['bitrate']) ? $this->data['bitrate'] : '');
-        $this->result['format'] = (isset($this->data['fileformat']) ? $this->data['fileformat'] : '');
-        $this->result['genre'] = (isset($this->data['tags_html']['id3v2']['genre'][0]) ? $this->data['tags_html']['id3v2']['genre'][0] : '');
-        $this->result['md5'] = md5_file($this->fileToScan);
-        $this->result['path'] = (isset($this->data['filepath']) ? $this->data['filepath'] : '');
+        $this->result['filename']         = (isset($this->data['filename']) ? $this->data['filename'] : '' );
+        $this->result['album']            = (isset($this->data['tags_html']['id3v2']['album'][0]) ? $this->data['tags_html']['id3v2']['album'][0] : '');
+        $this->result['artist']           = (isset($this->data['tags_html']['id3v2']['artist'][0]) ? $this->data['tags_html']['id3v2']['artist'][0] : '');
+        $this->result['bitrate']          = (isset($this->data['bitrate']) ? $this->data['bitrate'] : '');
+        $this->result['format']           = (isset($this->data['fileformat']) ? $this->data['fileformat'] : '');
+        $this->result['genre']            = (isset($this->data['tags_html']['id3v2']['genre'][0]) ? $this->data['tags_html']['id3v2']['genre'][0] : '');
+        $this->result['md5']              = md5_file($this->fileToScan);
+        $this->result['path']             = (isset($this->data['filepath']) ? $this->data['filepath'] : '');
         $this->result['playtime_seconds'] = (isset($this->data['playtime_seconds']) ? $this->data['playtime_seconds'] : '');
-        $this->result['playtime_string'] = (isset($this->data['playtime_string']) ? $this->data['playtime_string'] : '');
-        $this->result['title'] = (isset($this->data['tags_html']['id3v2']['title'][0]) ? $this->data['tags_html']['id3v2']['title'][0] : '');
-        $this->result['track_number'] = (isset($this->data['tags_html']['id3v2']['track_number'][0]) ? $this->data['tags_html']['id3v2']['track_number'][0] : NULL);
-        $this->result['year'] = (isset($this->data['tags_html']['id3v2']['year'][0]) ? $this->data['tags_html']['id3v2']['year'][0] : '');
+        $this->result['playtime_string']  = (isset($this->data['playtime_string']) ? $this->data['playtime_string'] : '');
+        $this->result['title']            = (isset($this->data['tags_html']['id3v2']['title'][0]) ? $this->data['tags_html']['id3v2']['title'][0] : '');
+        $this->result['track_number']     = (isset($this->data['tags_html']['id3v2']['track_number'][0]) ? $this->data['tags_html']['id3v2']['track_number'][0] : NULL);
+        $this->result['year']             = (isset($this->data['tags_html']['id3v2']['year'][0]) ? $this->data['tags_html']['id3v2']['year'][0] : '');
 
         $entity = $this->createEntity($this->result);
 
@@ -81,7 +82,8 @@ class Music_Model_Mapper_TagInfo extends Jgs_Model_Mapper_Abstract
         if (!empty($ThisFileInfo['comments']['picture'])) {
             foreach ($ThisFileInfo['comments']['picture'] as $key => $valuearray) {
                 if (isset($valuearray['data']) && isset($valuearray['image_mime'])) {
-                    $file = $path . str_replace(':','',$ThisFileInfo['tags']['id3v2']['album'][0]) . '.' . str_replace('image/', '', $valuearray['image_mime']);
+                    $file = $path . str_replace(':', '', $ThisFileInfo['tags']['id3v2']['album'][0])
+                        . '.' . str_replace('image/', '', $valuearray['image_mime']);
                     if (!is_readable($file)) {
                         file_put_contents($file, $valuearray['data']);
                     } else {
